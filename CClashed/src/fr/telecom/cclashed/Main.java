ad34f05066b294fcd7b5c00f2f0a5db490c27e05
Myouf62 <emilienhenon@gmail.com>
2017-03-21 14:30:52 +0100

    Modif du Main

--- CClashed/src/fr/telecom/cclashed/Main.java
+++ CClashed/src/fr/telecom/cclashed/Main.java
@@ -10,9 +10,9 @@ public class Main {
 	final static int largeur = 600;
     final static int hauteur = 600;
     final static int tailleC = 60;
-    static Fenetre f = new Fenetre ("Cclashed", largeur, hauteur);
+    static Fenetre f = new Fenetre ("CClashed", largeur, hauteur);
     
-    private final static Clavier clavier = new Clavier();
+    //private final static Clavier clavier = new Clavier();
     private final static Souris souris = new Souris(600);
     
 	public static void main(String[] args) {
@@ -40,118 +40,107 @@ public class Main {
 		//plateau.addCandy(redCandy);
 		//System.out.println(plateau);*/
 		
-		/* utilisaiton souris et clavier */
+		/* Utilisation souris et clavier */
 		f.addMouseListener(souris);
 		f.getP().addMouseListener(souris);
 		f.getP().addMouseMotionListener(souris);
 
-		/* creation du plateau */
+		/* Création du plateau de jeu */
 		Board plateau = new Board(10,10); 
 		Point p;
 
-		/* gestion facile de l'affichage des cases */
+		/* Gestion facile de l'affichage des cases */
 		Carre cases[][] = new Carre[10][10];
 
-		/* points qui permettront de recuperer les deux cases a inverser */
+		/* Points qui permettront de récupérer les deux cases à inverser */
 		Point posS1 = new Point(0,0);
 		Point posS2 = new Point(0,0);
 		int x1, y1, x2, y2;
 
-		/* affichage du plateau une premiere fois */
-		for (int i = 0; i < 10; i++){
-		    for (int j = 0; j < 10; j++){
-			switch(plateau.getCandy(i,j).getColor()){
-			case 0:
-			    p = new Point(i*tailleC,j*tailleC);
-			    cases[i][j] = new Carre(Color.white,p,tailleC,true);
-			    f.ajouter(cases[i][j]);
-			    break;
+		/* Affichage du plateau une première fois */
+		for (int i = 0; i < 10; i++) {
+		    for (int j = 0; j < 10; j++) {
+			switch(plateau.getCandy(i,j).getColor()) {
+				case 0:
+					p = new Point(i*tailleC,j*tailleC);
+					cases[i][j] = new Carre(Color.WHITE,p,tailleC,true);
+					f.ajouter(cases[i][j]);
+					break;
 
-			case 1:
-			    p = new Point(i*tailleC,j*tailleC);
-			    cases[i][j] = new Carre(Color.red,p,tailleC,true);
-			    f.ajouter(cases[i][j]);
-			    break;
+				case 1:
+					p = new Point(i*tailleC,j*tailleC);
+					cases[i][j] = new Carre(Color.ORANGE,p,tailleC,true);
+					f.ajouter(cases[i][j]);
+					break;
 
-			case 2:
-			    p = new Point(i*tailleC,j*tailleC);
-			    cases[i][j] = new Carre(Color.green,p,tailleC,true);
-			    f.ajouter(cases[i][j]);
-			    break;
+				case 2:
+					p = new Point(i*tailleC,j*tailleC);
+					cases[i][j] = new Carre(Color.GREEN,p,tailleC,true);
+			    	f.ajouter(cases[i][j]);
+			    	break;
 
-			case 3:    
-			    p = new Point(i*tailleC,j*tailleC);
-			    cases[i][j] = new Carre(Color.blue,p,tailleC,true);
-			    f.ajouter(cases[i][j]);
-			    break;
+				case 3:    
+					p = new Point(i*tailleC,j*tailleC);
+					cases[i][j] = new Carre(Color.YELLOW,p,tailleC,true);
+					f.ajouter(cases[i][j]);
+					break;
 
-			case 4:
-			    p = new Point(i*tailleC,j*tailleC);
-			    cases[i][j] = new Carre(Color.yellow,p,tailleC,true);
-			    f.ajouter(cases[i][j]);
-			    break;
+				case 4:
+					p = new Point(i*tailleC,j*tailleC);
+					cases[i][j] = new Carre(Color.RED,p,tailleC,true);
+					f.ajouter(cases[i][j]);
+					break;
 
-			case 5:
-			    p = new Point(i*tailleC,j*tailleC);
-			    cases[i][j] = new Carre(Color.magenta,p,tailleC,true);
-			    f.ajouter(cases[i][j]);
-			    break;
+				case 5:
+					p = new Point(i*tailleC,j*tailleC);
+					cases[i][j] = new Carre(new Color(193,154,107),p,tailleC,true);
+					f.ajouter(cases[i][j]);
+					break;
 
-			case 6 :
-			    p = new Point(i*tailleC,j*tailleC);
-			    cases[i][j] = new Carre(Color.orange,p,tailleC,true);
-			    f.ajouter(cases[i][j]);
-			    break;
-
-			}
+				}
 		    }
 		}
 
-		while (true){
-		    /* on eclate plusieurs fois pour ne pas avoir de vide au debut */
-		    for (int i = 0; i < 25; i++){
-			plateau.eclater();
+		while (true) {
+		    /* On éclate plusieurs fois pour ne pas avoir de vide au début */
+		    for (int i = 0; i < 25; i++) {
+		    	plateau.eclater();
 		    }	
 		    f.rafraichir();
 
-		    /* au fur et a mesure des passages dans la boucle infinie, les deux boucles et le switch vont permettre d'actualiser la couleur de la bulle en fonction de la valeur qu'elle contient */
-		    for (int i = 0; i < 10; i++){
-			for (int j = 0; j < 10; j++){
-			    switch(plateau.getCandy(i,j).getColor()){
-			    case 0:
-				cases[i][j].setCouleur(Color.white);
-				break;
+		    /* Au fur et à mesure des passages dans la boucle infinie, les deux boucles et le switch vont permettre d'actualiser la couleur de la bulle en fonction de la valeur qu'elle contient */
+		    for (int i = 0; i < 10; i++) {
+		    	for (int j = 0; j < 10; j++) {
+		    		switch(plateau.getCandy(i,j).getColor()){
+		    			case 0:
+		    				cases[i][j].setCouleur(Color.WHITE);
+		    				break;
 
-			    case 1:
-				cases[i][j].setCouleur(Color.red);
-				break;
+		    			case 1:
+		    				cases[i][j].setCouleur(Color.ORANGE);
+		    				break;
 
-			    case 2:
-				cases[i][j].setCouleur(Color.green);
-				break;
+		    			case 2:
+		    				cases[i][j].setCouleur(Color.GREEN);
+		    				break;
 
-			    case 3:    
-				cases[i][j].setCouleur(Color.blue);  
-				break;
+		    			case 3:    
+		    				cases[i][j].setCouleur(Color.YELLOW);  
+		    				break;
 
-			    case 4:
-				cases[i][j].setCouleur(Color.yellow);
-				break;
+		    			case 4:
+		    				cases[i][j].setCouleur(Color.RED);
+		    				break;
 
-			    case 5:
-				cases[i][j].setCouleur(Color.magenta);
-				break;
-
-			    case 6 :
-				cases[i][j].setCouleur(Color.orange);
-				break;
-
-			    }
+		    			case 5:
+		    				cases[i][j].setCouleur(new Color(193,154,107));
+		    				break;
+		    		}
 			    f.rafraichir();
-			}
+		    	}
 		    }
 
-		    /* jeu en pause tant qu'il n'y a pas de clic */
+		    /* Jeu en pause tant qu'il n'y a pas de clic */
 		    while(!souris.getClicGauche())
 			try{Thread.sleep(0,5);}catch(Exception e){} // pour ralentir un peu l'application
 		    posS1=souris.getPosition();
@@ -164,10 +153,8 @@ public class Main {
 		    x2 = (int)((posS2.getX())/tailleC);
 		    y2 = (int)((posS2.getY())/tailleC);
 
-		    /* on verifie que les cases sont voisines pour inverser leurs valeurs */
-		    if (((x1+1 == x2 || x1-1 == x2) 
-			 && (y1 == y2)) || ((y1+1 == y2 || y1-1 == y2)) 
-			&& (x1 == x2))
+		    /* On vérifie que les bonbons sont voisins avant inversion */
+		    if (((x1+1 == x2 || x1-1 == x2) && (y1 == y2)) || ((y1+1 == y2 || y1-1 == y2)) && (x1 == x2))
 			//insérer ici méthode pour switcher 2 points
 		    f.rafraichir();
 		}
